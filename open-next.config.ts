cd746406a9edb21fe098f8de990d6b72f559db36
Zeng Zigao <168578317+zzgtylor@users.noreply.github.com>
2026-06-19 14:22:31 -0700

    Add edgeExternals configuration for crypto module

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,18 +5,22 @@ const config: OpenNextConfig = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
     },
   },
-
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
