d0b5c08558ba4475fbdb5c14df9e7c3cca376266
Zeng Zigao <168578317+zzgtylor@users.noreply.github.com>
2026-06-23 16:07:52 -0700

    Delete src/app/api/keystatic/[...params]/route.ts

--- src/app/api/keystatic/[...params]/route.ts
+++
@@ -1,8 +0,0 @@
-import { makeRouteHandler } from '@keystatic/next/route-handler'
-import config from '../../../../../keystatic.config'
-
-// Keystatic's API handler performs filesystem/git operations and must run on
-// the Node.js runtime, not the edge/Workers runtime.
-export const runtime = 'nodejs'
-
-export const { POST, GET } = makeRouteHandler({ config })
